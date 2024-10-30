ef3e1d6ee6e2b4383a4030abb8f50035d2f170d2
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2024-10-30 11:33:05 +0200

    + Workaround for Joomla! 5.2 broken mail template layout

--- component/backend/src/Helper/TemplateEmails.php
+++ component/backend/src/Helper/TemplateEmails.php
@@ -9,6 +9,7 @@ namespace Akeeba\Component\Engage\Administrator\Helper;
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Mail\Mail;
 use Joomla\CMS\Mail\MailTemplate;
@@ -327,7 +328,15 @@ abstract class TemplateEmails
 				return false;
 			}
 
-			$templateMailer = new MailTemplate($key, $langTag, $mailer);
+			if (ComponentHelper::getParams('com_engage')->get('workaround_mailtemplate', 1))
+			{
+				$templateMailer = MailTemplateHotFix::getAWorkingMailTemplate($key, $langTag, $mailer);
+			}
+			else
+			{
+				$templateMailer = new MailTemplate($key, $langTag, $mailer);
+			}
+
 			$templateMailer->addTemplateData($data);
 			$templateMailer->addRecipient(trim($user->email), $user->name);
 
